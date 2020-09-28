4c2ec23ca884b782769614f3b52b6bf784080b6e
srikanth4c <srikanthbinf@gmail.com>
2020-09-28 11:04:55 +0530

    footer added

--- instalMojo_clone/script.js
+++ instalMojo_clone/script.js
@@ -1,6 +1,9 @@
 const menu= document.getElementById('menu');
 const side_nav= document.getElementById('side-nav');
-const main_container=document.getElementById('main-container')
+const main_container=document.getElementById('main-container');
+const nav_link= document.querySelectorAll('.nav-link');
+const submenu_collapse= document.querySelectorAll('.submenu-collapse');
+
 menu.addEventListener('click', ()=>{
      side_nav.classList.add("side-nav-collapse")
      bool=false;
@@ -11,5 +14,16 @@ document.getElementById('collapse-side').addEventListener('click', ()=>{
     bool=true;
 })
 
+console.log(nav_link)
 
-   
+nav_link.forEach((link, ind)=>{
+    if(ind!=4){
+        console.log(ind)
+        link.addEventListener('mouseenter', ()=>{
+            submenu_collapse[ind].classList.add('submenu')
+        })
+        link.addEventListener('mouseleave', ()=>{
+            submenu_collapse[ind].classList.remove('submenu')
+        })
+    }
+})
